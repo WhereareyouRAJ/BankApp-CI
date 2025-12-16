0e200133f107568b232796ddcd6281aa1c5d1b47
Raj singh <rajx4684@gmail.com>
2025-12-16 15:54:31 +0530

    Update BankappApplicationTests.java

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -2,19 +2,18 @@ package com.example.bankapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootTest(
-    exclude = {
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {
         DataSourceAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class
-    }
-)
+})
 class BankappApplicationTests {
 
     @Test
     void contextLoads() {
-        // Context loads without DB
     }
 }
